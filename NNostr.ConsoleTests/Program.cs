bdda1903de7fe3adb9efeb418fbb4bc3e3a12286
Gabriel Aponte HOME <gabriel.aponte@smart-side.com>
2023-09-20 13:39:02 +0200

    Add Subscribe to Notices

--- NNostr.ConsoleTests/Program.cs
+++ NNostr.ConsoleTests/Program.cs
@@ -5,7 +5,8 @@ using NNostr.Client;
 // Load a private key from a hex string
 var testPrivKey = "0c0cf4dba557094eea6341e8f0b0b7eaf71d90a465104359bf930705676d4d3f";
 var testPublicKey = "381af99c0282f71df21fa57d38bf7d2cb8f8609fc3ffe3117ec5236d8a99fea2";
-var relayForTest = new Uri("wss://nos.lol");
+//var relayForTest = new Uri("wss://localhost:5001");
+var relayForTest = new Uri("wss://pretty-jaybird-causal.ngrok-free.app");
 
 // TODO:  Fix Readme if this works
 var client = new NostrClient(relayForTest);
@@ -15,6 +16,7 @@ var thisSubsId = Guid.NewGuid().ToString();
 Console.WriteLine($"Starting NNostr Client. Subscription: {thisSubsId}");
 Console.WriteLine("Press 'ESC' to exit the process...");
 
+SubscribeToNotices(client);
 await CreateSubscription(testPublicKey, client, thisSubsId);
 SubscribeToEvents(client, thisSubsId);
 
@@ -56,11 +58,20 @@ static void SubscribeToEvents(NostrClient? client, string thisSubsId)
         {
             foreach (var nostrEvent in args.events)
             {
-                Console.WriteLine(nostrEvent.Content);
+                Console.WriteLine($"Event: {nostrEvent.Content}");
             }
         }
     }
     client.EventsReceived += OnClientOnEventsReceived;
+}
+
+static void SubscribeToNotices(NostrClient? client)
+{
+    void OnClientOnNoticeReceived(object sender, string notice)
+    {
+        Console.WriteLine($"Notice: {notice}");
+    }
+    client.NoticeReceived += OnClientOnNoticeReceived;
 }
 
 static async Task CreateSubscription(string testPublicKey, NostrClient client, string thisSubsId)
